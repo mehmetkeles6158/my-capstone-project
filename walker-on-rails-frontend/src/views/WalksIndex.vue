50674ea5720bf0efc3e8ba3ce50355fa4ecf9148
Mehmet <mehmetk6158@gmail.com>
2021-12-13 20:08:31 -0500

    add theme for index action on the frontend

--- walker-on-rails-frontend/src/views/WalksIndex.vue
+++ walker-on-rails-frontend/src/views/WalksIndex.vue
@@ -1,21 +1,73 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <div v-for="walk in walks">
-      <p>
-        <b>User id:</b>
-        {{ walk.user_id }}
-      </p>
-      <p>
-        <b>Date:</b>
-        {{ walk.date }}
-      </p>
-      <p>
-        <b>Steps:</b>
-        {{ walk.steps }}
-      </p>
-      <router-link v-bind:to="`walks/${walk.id}`">More Info</router-link>
-      <hr />
+    <div class="card text-white bg-secondary my-5 py-4 text-center">
+      <div class="card-body">
+        <h2 class="text-white m-0">Walk Distances</h2>
+      </div>
+    </div>
+    <div class="container px-4 px-lg-5">
+      <!-- Heading Row-->
+      <div class="row gx-4 gx-lg-5 align-items-center my-5">
+        <div class="col-lg-7">
+          <img
+            class="img-fluid rounded mb-4 mb-lg-0"
+            src="https://images.unsplash.com/photo-1449459831403-2ea2681d4270?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&w=1000&q=80"
+            alt="..."
+          />
+        </div>
+        <!-- <div class="col-lg-5">
+          <h1 class="font-weight-light">Your Walk Distances</h1>
+        </div> -->
+      </div>
+      <!-- Call to Action-->
+
+      <!-- Content Row-->
+      <div class="row gx-4 gx-lg-5" v-for="walk in walks">
+        <div class="col-md-4 mb-5">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Walk Info</h2>
+              <p class="card-text">
+                <b>User id:</b>
+                {{ walk.user_id }}
+                <br />
+                <b>Date:</b>
+                {{ walk.date }}
+                <br />
+                <b>Steps:</b>
+                {{ walk.steps }}
+              </p>
+            </div>
+            <div class="card-footer">
+              <a class="btn btn-primary btn-sm" v-bind:href="`/walks/${walk.id}`">More Info</a>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="col-md-4 mb-5">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Card Two</h2>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim!
+                Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.
+              </p>
+            </div>
+            <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+          </div>
+        </div> -->
+        <!-- <div class="col-md-4 mb-5">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Card Three</h2>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam
+                molestias corporis quod, ea minima accusamus.
+              </p>
+            </div>
+            <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+          </div>
+        </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -27,7 +79,7 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Your Distances",
+      message: "Daily Walks",
       walks: [],
     };
   },
