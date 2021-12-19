a4cd0a07302651e217a588079a5565f558eed880
Mehmet <mehmetk6158@gmail.com>
2021-12-18 22:59:51 -0500

    some edits

--- walker-on-rails-frontend/src/views/About.vue
+++ walker-on-rails-frontend/src/views/About.vue
@@ -1,27 +1,21 @@
 <template>
   <div class="about">
-    <div class="card" style="width: 18rem">
-      <img
-        src="https://media.istockphoto.com/photos/tourist-with-backpack-at-mountain-lake-background-picture-id1277142237?b=1&k=20&m=1277142237&s=170667a&w=0&h=AgXsgbj033fxbWYrSkSh1kWGfyBzTA3zxl9-CjRHuls="
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-      </div>
-    </div>
-    <div class="card" style="width: 18rem">
-      <img
-        src="https://media.istockphoto.com/photos/tourist-with-backpack-at-mountain-lake-background-picture-id1277142237?b=1&k=20&m=1277142237&s=170667a&w=0&h=AgXsgbj033fxbWYrSkSh1kWGfyBzTA3zxl9-CjRHuls="
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
+    <div class="container px-6 px-lg-6">
+      <div class="row gx-6 gx-lg-6">
+        <div class="col-md-12 mb-6 justify-content-center">
+          <div class="card" style="width: 40rem">
+            <img
+              src="https://media.istockphoto.com/photos/tourist-with-backpack-at-mountain-lake-background-picture-id1277142237?b=1&k=20&m=1277142237&s=170667a&w=0&h=AgXsgbj033fxbWYrSkSh1kWGfyBzTA3zxl9-CjRHuls="
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
