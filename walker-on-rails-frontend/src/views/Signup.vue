042caad47ee319b79507968930d5f86742f2a44d
Mehmet <mehmetk6158@gmail.com>
2021-12-19 17:32:38 -0500

    style signup page

--- walker-on-rails-frontend/src/views/Signup.vue
+++ walker-on-rails-frontend/src/views/Signup.vue
@@ -1,6 +1,79 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
+    <section
+      class="h-100 bg-image"
+      style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.jpg')"
+    >
+      <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div class="card" style="border-radius: 15px">
+                <div class="card-body p-3">
+                  <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+                  <ul>
+                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+                  </ul>
+                  <form v-on:submit.prevent="submit()">
+                    <div class="form-outline mb-4">
+                      <input
+                        type="text"
+                        v-model="newUserParams.name"
+                        id="form3Example1cg"
+                        class="form-control form-control-lg"
+                        placeholder="Name"
+                      />
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input
+                        type="email"
+                        v-model="newUserParams.email"
+                        id="form3Example3cg"
+                        class="form-control form-control-lg"
+                        placeholder="Email"
+                      />
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input
+                        type="password"
+                        v-model="newUserParams.password"
+                        id="form3Example4cg"
+                        class="form-control form-control-lg"
+                        placeholder="Password"
+                      />
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input
+                        type="password"
+                        v-model="newUserParams.password_confirmation"
+                        id="form3Example4cdg"
+                        class="form-control form-control-lg"
+                        placeholder="Confirm Password"
+                      />
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                      <button type="button" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">
+                        Register
+                      </button>
+                    </div>
+
+                    <p class="text-center text-muted mt-5 mb-0">
+                      Have already an account?
+                      <a href="#!" class="fw-bold text-body"><u>Login here</u></a>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- <form v-on:submit.prevent="submit()">
       <h1>Signup</h1>
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
@@ -22,7 +95,7 @@
         <input type="password" v-model="newUserParams.password_confirmation" />
       </div>
       <input type="submit" value="Submit" />
-    </form>
+    </form> -->
   </div>
 </template>
 
