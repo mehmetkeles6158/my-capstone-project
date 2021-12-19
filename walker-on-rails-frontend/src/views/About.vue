4b06bb6d3fa345d5c8c18a076b6b94082af2f0e2
Mehmet <mehmetk6158@gmail.com>
2021-12-18 22:50:56 -0500

    style about page

--- walker-on-rails-frontend/src/views/About.vue
+++ walker-on-rails-frontend/src/views/About.vue
@@ -1,3 +1,28 @@
 <template>
-  <div class="about"></div>
+  <div class="about">
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://media.istockphoto.com/photos/tourist-with-backpack-at-mountain-lake-background-picture-id1277142237?b=1&k=20&m=1277142237&s=170667a&w=0&h=AgXsgbj033fxbWYrSkSh1kWGfyBzTA3zxl9-CjRHuls="
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://media.istockphoto.com/photos/tourist-with-backpack-at-mountain-lake-background-picture-id1277142237?b=1&k=20&m=1277142237&s=170667a&w=0&h=AgXsgbj033fxbWYrSkSh1kWGfyBzTA3zxl9-CjRHuls="
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
