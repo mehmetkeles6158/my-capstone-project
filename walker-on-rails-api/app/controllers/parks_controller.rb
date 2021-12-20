f3de3ae72ac2e6049e0e6c57a0ccd6ebd90217f4
Mehmet <mehmetk6158@gmail.com>
2021-12-20 11:12:07 -0500

    enyc park api key

--- walker-on-rails-api/app/controllers/parks_controller.rb
+++ walker-on-rails-api/app/controllers/parks_controller.rb
@@ -1,6 +1,6 @@
 class ParksController < ApplicationController
   def index
-    response = HTTP.get("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=hqEy2jdA2hNCsDg6NpPeeedojOnkEWfiRuDlndfy")
+    response = HTTP.get("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=#{Rails.application.credentials.park_api_key}")
     render json: response.parse
   end
 end
--- walker-on-rails-api/app/controllers/weathers_controller.rb
+++ walker-on-rails-api/app/controllers/weathers_controller.rb
@@ -1,6 +1,2 @@
 class WeathersController < ApplicationController
-  # def index
-  #   response = HTTP.get("api.openweathermap.org/data/2.5/weather?zip=44138,us&appid=b924987f8983ed94c3bf2113bf3e95d8")
-  #   render json: response.parse
-  # end
 end
